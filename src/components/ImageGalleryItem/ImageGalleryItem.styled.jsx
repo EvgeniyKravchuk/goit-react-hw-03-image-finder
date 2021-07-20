a4ec16c84c7278f0bb42c082a328fa0dbdaed062
EvgeniyKravchuk <gekahhh1996@gmail.com>
2021-07-20 22:14:26 +0300

    finally

---
+++ src/components/ImageGalleryItem/ImageGalleryItem.styled.jsx
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+
+export const Item = styled.li`
+  width: 330px;
+  height: 200px;
+  margin-bottom: 10px;
+`;
+
+export const Image = styled.img`
+  width: 100%;
+  height: 100%;
+`;
